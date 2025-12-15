19d0fcc60ecb45b887ff61d08bf858c4813d7337
Rama Harrison <ramaliu2013@gmail.com>
2025-12-16 04:17:01 +0700

    Delete OOP_Project_Laporan/KebunBinatang directory

--- OOP_Project_Laporan/KebunBinatang/Herbivora.java
+++
@@ -1,8 +0,0 @@
-package KebunBinatang;
-
-class Herbivora extends Karnivora {
-    public Herbivora(String namahewan, String makanan, String habitat, String deskripsi, String suara,
-            String kegiatan) {
-        super(namahewan, makanan, habitat, deskripsi, suara, kegiatan);
-    }
-}
